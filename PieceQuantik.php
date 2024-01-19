b83d469fd7c3c327a75eda74d9f7f996e3d8e841
Syrder Baptichon <syrder.baptichon@etu.univ-lehavre.fr>
2024-01-19 16:03:08 +0100

    Création de la classe PieceQuantik.php

---
+++ PieceQuantik.php
@@ -0,0 +1,130 @@
+<?php
+class PieceQuantik
+{
+    public static int $WHITE = 0;
+    public static int $BLACK = 1;
+    public static int $VOID = 0;
+    public static int $CUBE = 1;
+    public static int $CONE = 2;
+    public static int $CYLINDRE = 3;
+    public static int $SPHERE = 4;
+
+    protected int $forme;
+    protected int $couleur;
+
+    private function __construct(int $forme, int $couleur)
+    {
+        $this->forme = $forme;
+        $this->couleur = $couleur;
+    }
+
+    /**
+     * @return int
+     */
+    public function getForme(): int
+    {
+        return $this->forme;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCouleur(): int
+    {
+        return $this->couleur;
+    }
+
+    public function __toString(): String
+    {
+        // TODO: Implement __toString() method.
+        $chaine = "(";
+        switch ($this->forme) {
+            case "0":
+                $chaine.="&nbsp;&nbsp;&nbsp;&nbsp;";
+                break;
+            case "1":
+                $chaine.="Co:";
+                break;
+            case "2":
+                $chaine.="Cu:";
+                break;
+            case "3":
+                $chaine.="Cy:";
+                break;
+            case "4":
+                $chaine.="Sp:";
+                break;
+            default:
+                $chaine.="";
+        }
+        if($this->forme != self::$VOID) {
+            switch ($this->couleur) {
+                case "0":
+                    $chaine.="W";
+                    break;
+                case "1":
+                    $chaine.="B";
+                    break;
+                default:
+                    $chaine.="";
+            }
+        }
+        return $chaine.")";
+    }
+
+    public static function initVoid(): PieceQuantik
+    {
+        return new PieceQuantik(self::$VOID, self::$WHITE);
+    }
+
+    public static function initWhiteCube(): PieceQuantik
+    {
+        return new PieceQuantik(self::$CUBE, self::$WHITE);
+    }
+    public static function initBlackCube(): PieceQuantik
+    {
+        return new PieceQuantik(self::$CUBE, self::$BLACK);
+    }
+
+    public static function initWhiteCone(): PieceQuantik
+    {
+        return new PieceQuantik(self::$CONE, self::$WHITE);
+    }
+    public static function initBlackCone(): PieceQuantik
+    {
+        return new PieceQuantik(self::$CONE, self::$BLACK);
+    }
+
+    public static function initWhiteCylindre(): PieceQuantik
+    {
+        return new PieceQuantik(self::$CYLINDRE, self::$WHITE);
+    }
+    public static function initBlackCylindre(): PieceQuantik
+    {
+        return new PieceQuantik(self::$CYLINDRE, self::$BLACK);
+    }
+
+    public static function initWhiteSphere(): PieceQuantik
+    {
+        return new PieceQuantik(self::$SPHERE, self::$WHITE);
+
+    }
+    public static function initBlacksphere(): PieceQuantik
+    {
+        return new PieceQuantik(self::$SPHERE, self::$BLACK);
+
+    }
+}
+
+echo "<pre>\n";
+echo PieceQuantik::initBlackCone()."<br />\n";
+echo PieceQuantik::initWhiteCone()."<br />\n";
+echo PieceQuantik::initBlackCube()."<br />\n";
+echo PieceQuantik::initWhiteCube()."<br />\n";
+echo PieceQuantik::initBlackCylindre()."<br />\n";
+echo PieceQuantik::initWhiteCylindre()."<br />\n";
+echo PieceQuantik::initBlackSphere()."<br />\n";
+echo PieceQuantik::initWhiteSphere()."<br />\n";
+echo PieceQuantik::initVOID()."<br />\n";
+echo "</pre>\n";
+?>
